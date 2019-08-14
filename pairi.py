f462de9b7e4298fb5fb7340eaf72231739e66a31
Lucas Sweany <39360190+sweany@users.noreply.github.com>
2019-08-14 04:12:24 +0000

    less debug output

--- pairi.py
+++ pairi.py
@@ -19,39 +19,35 @@ fh = open(logfile, 'a')
 
 class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
     def do_GET(self):
-        ts = datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S%z")
-        print(self.command, self.path, self.headers)
+        ts = datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%SZ")
+        print(self.client_address, self.command, self.path)
         bits = urlparse(self.path)
         #print(bits.query)
         data = parse_qs(bits.query)
-        print(json.dumps(data))
+        #print(json.dumps(data))
 
         self.send_response(200)
         self.end_headers()
-        self.wfile.write(b'Hello, world!')
+        #self.wfile.write(b'OK')
 
-        # write data to log file
-        print()
+        # write data to log file, put dict keys in order
         data['datetime'] = ts
         o = OrderedDict(data)
         o.move_to_end('key', last=False)
         o.move_to_end('datetime', last=False)
         fh.write(json.dumps(o) + "\n")
         fh.flush()
-        
 
         # forward the request on to the real destination
         headers = { 'Host': 'api.thingspeak.com', 'User-Agent': 'PurpleAir/4.02' }
         url = 'https://34.226.171.107' + self.path
-        print('forwarding request to ' + url)
+        #print('forwarding request to ' + url)
         try:
             r = requests.get(url, headers=headers, verify=False)
         except BaseException as e:
             print("Exception: {}".format(e))
         
-        print(r.status_code)
-        print(r.text)
-
+        print(r.status_code, r.text)
         print()
 
 #httpd = HTTPServer(('0.0.0.0', 443), BaseHTTPRequestHandler)
